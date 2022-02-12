2539ffea44071bef51f6858b95e9520b92e92ba0
Fabian Schomerus <>
2022-02-12 16:07:54 +0000

    update names in main

--- news_data_scraper/main.py
+++ news_data_scraper/main.py
@@ -6,23 +6,23 @@ from news_data_scraper.document.create_document_object import create_document_co
 from news_data_scraper.document.export.word.format_document import export_to_word
 from news_data_scraper.scraper.scrape_article import scrape_all_urls
 
-DIRECTORY_ROOT = Path(__file__).parent.parent
-INPUT_FILE_PATH = "input/DataScraperINPUT.txt"
-OUTPUT_FILE_PATH = "output/NewsDataScraperOutput.docx"
+_DIRECTORY_ROOT = Path(__file__).parent.parent
+_INPUT_FILE_PATH = "input/DataScraperINPUT.txt"
+_OUTPUT_FILE_PATH = "output/NewsDataScraperOutput.docx"
 
 
 def document_creator(run_cga: bool) -> None:
     """
-    Web scraper that creates a word document from url inputs in excel, scrapes title, date and 1st 3 sentences
+    Web scraper that creates a word document from url inputs in txt, scrapes title, date and 1st 3 sentences
     :return: document
     """
     setup_logging()
 
     # read excel and create dictionaries of R&N and CGA urls which will be scraped
-    input_file_path = DIRECTORY_ROOT / INPUT_FILE_PATH
+    input_file_path = _DIRECTORY_ROOT / _INPUT_FILE_PATH
     all_articles = scrape_all_urls(input_file_path, run_cga)
 
-    output_file_name = create_document_name(OUTPUT_FILE_PATH)
+    output_file_name = create_document_name(_OUTPUT_FILE_PATH)
     document_contents = create_document_contents(all_articles, output_file_name, run_cga)
 
     full_output_file_path = create_document_filepath(output_file_name)
@@ -37,7 +37,7 @@ def create_document_name(output_file_name: str) -> str:
 
 
 def create_document_filepath(output_file_name: str) -> Path:
-    return Path(f"{DIRECTORY_ROOT}/{output_file_name}")
+    return Path(f"{_DIRECTORY_ROOT}/{output_file_name}")
 
 
 if __name__ == '__main__':
